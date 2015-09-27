d99b53cfd079be08b41c0ebef9b5e57ca0f8de3d
Nikola Posa <posa.nikola@gmail.com>
2015-09-27 11:46:33 +0200

    Versionable interface/trait.

---
+++ src/VersionableInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the Version package.
+ *
+ * Copyright (c) Nikola Posa <posa.nikola@gmail.com>
+ *
+ * For full copyright and license information, please refer to the LICENSE file,
+ * located at the package root folder.
+ */
+
+namespace Version;
+
+/**
+ * @author Nikola Posa <posa.nikola@gmail.com>
+ */
+interface VersionableInterface
+{
+    /**
+     * @param Version $version
+     * @return void
+     */
+    public function setVersion(Version $version);
+
+    /**
+     * @return Version
+     */
+    public function getVersion();
+}
