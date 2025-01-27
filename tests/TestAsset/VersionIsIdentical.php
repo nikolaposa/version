496e4427c3e817e05c4cb4c745aa970a82bb1334
Dimitri <dimitri.guitton.dev@gmail.com>
2025-01-27 14:56:49 +0100

    refactor : update code to comply with repository standards

--- tests/TestAsset/VersionIsIdentical.php
+++ tests/TestAsset/VersionIsIdentical.php
@@ -1,55 +1,56 @@
 <?php
 
-    declare(strict_types=1);
+declare(strict_types=1);
 
-    namespace Version\Tests\TestAsset;
+namespace Version\Tests\TestAsset;
 
-    use PHPUnit\Framework\Constraint\Constraint;
-    use Version\Extension\Build;
-    use Version\Extension\PreRelease;
-    use Version\Version;
+use PHPUnit\Framework\Constraint\Constraint;
+use Version\Extension\Build;
+use Version\Extension\PreRelease;
+use Version\Version;
 
-    final class VersionIsIdentical extends Constraint
+final class VersionIsIdentical extends Constraint
+{
+    private Version $expectedVersion;
+
+    public function __construct(
+        int                    $expectedMajor,
+        int                    $expectedMinor,
+        int                    $expectedPatch,
+        PreRelease|string|null $expectedPreRelease = null,
+        Build|string|null      $expectedBuild = null
+    )
     {
-        private Version $expectedVersion;
-
-        public function __construct(
-            int $expectedMajor,
-            int $expectedMinor,
-            int $expectedPatch,
-            PreRelease|string|null $expectedPreRelease = null,
-            Build|string|null $expectedBuild = null
-        ) {
-            $this->expectedVersion = Version::from(
-                $expectedMajor,
-                $expectedMinor,
-                $expectedPatch,
-                is_string($expectedPreRelease) ? PreRelease::fromString($expectedPreRelease) : $expectedPreRelease,
-                is_string($expectedBuild) ? Build::fromString($expectedBuild) : $expectedBuild
-            );
-        }
-
-        protected function matches($version): bool
-        {
-            /* @var $version Version */
-
-            return (
-                $version->getMajor() === $this->expectedVersion->getMajor()
-                && $version->getMinor() === $this->expectedVersion->getMinor()
-                && $version->getPatch() === $this->expectedVersion->getPatch()
-                && (
-                    ($version->isPreRelease() && $this->expectedVersion->isPreRelease() && $version->getPreRelease()->getIdentifiers() === $this->expectedVersion->getPreRelease()->getIdentifiers())
-                    || (!$version->isPreRelease() && !$this->expectedVersion->isPreRelease())
-                )
-                && (
-                    ($version->hasBuild() && $this->expectedVersion->hasBuild() && $version->getBuild()->getIdentifiers() === $this->expectedVersion->getBuild()->getIdentifiers())
-                    || (!$version->hasBuild() && !$this->expectedVersion->hasBuild())
-                )
-            );
-        }
-
-        public function toString(): string
-        {
-            return 'is identical to: ' . $this->expectedVersion->toString();
-        }
+        $this->expectedVersion = Version::from(
+            $expectedMajor,
+            $expectedMinor,
+            $expectedPatch,
+            is_string($expectedPreRelease) ? PreRelease::fromString($expectedPreRelease) : $expectedPreRelease,
+            is_string($expectedBuild) ? Build::fromString($expectedBuild) : $expectedBuild
+        );
     }
+
+    protected function matches($version): bool
+    {
+        /* @var $version Version */
+
+        return (
+            $version->getMajor() === $this->expectedVersion->getMajor()
+            && $version->getMinor() === $this->expectedVersion->getMinor()
+            && $version->getPatch() === $this->expectedVersion->getPatch()
+            && (
+                ($version->isPreRelease() && $this->expectedVersion->isPreRelease() && $version->getPreRelease()->getIdentifiers() === $this->expectedVersion->getPreRelease()->getIdentifiers())
+                || (!$version->isPreRelease() && !$this->expectedVersion->isPreRelease())
+            )
+            && (
+                ($version->hasBuild() && $this->expectedVersion->hasBuild() && $version->getBuild()->getIdentifiers() === $this->expectedVersion->getBuild()->getIdentifiers())
+                || (!$version->hasBuild() && !$this->expectedVersion->hasBuild())
+            )
+        );
+    }
+
+    public function toString(): string
+    {
+        return 'is identical to: ' . $this->expectedVersion->toString();
+    }
+}
